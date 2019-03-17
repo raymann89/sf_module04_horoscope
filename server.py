4b095a93cfa392e15e414e05896b30bd2150bfd0
raymann89 <48382803+raymann89@users.noreply.github.com>
2019-03-17 17:12:37 +0300

    Add file for horoscope

---
+++ server.py
@@ -0,0 +1,30 @@
+from bottle import route, run, view
+from datetime import datetime as dt
+from random import random
+from horoscope04 import generate_prophecies
+
+
+@route("/")
+@view("predictions")
+def index():
+    now = dt.now()
+    x = random()
+
+    return {
+        "date": f"{now.year}-{now.month}-{now.day}",
+        "predictions": generate_prophecies(6, 2),
+        "special_date": x > 0.5,
+        "x": x
+    }
+
+
+@route("/api/forecast")
+def api_test():
+    return {"prophecies": generate_prophecies(6, 2)}
+
+
+run(
+    host="localhost",
+    port=8080,
+    autoreloaded=True
+)
